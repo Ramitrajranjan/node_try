ccaef73c379065d322b9c98b0c588c83f53bda15
Ramit Ranjan <ramitrajranjan@gamil.com>
2023-08-02 11:57:45 +0530

    Performed CRUD operation in file system in sync mode

---
+++ fs/index.js
@@ -0,0 +1,15 @@
+const fs= require('fs');
+// fs.mkdirSync("ramit");
+
+// fs.writeFileSync('ramit/bio.txt',"My name is Ramit Ranjan");
+
+// fs.appendFileSync('ramit/bio.txt',"Completed my post graduation from NITP");
+
+// const data=fs.readFileSync('ramit/bio.txt',"utf-8");
+// console.log(data);
+
+// fs.renameSync('ramit/bio.txt','myBio.txt');
+
+// fs.unlinkSync("myBio.txt");
+
+// fs.rmdirSync("ramit");
