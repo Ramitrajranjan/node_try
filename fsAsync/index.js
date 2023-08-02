a81735a1e29d4eb4ef5162cedfe87662c83f27fe
Ramit Ranjan <ramitrajranjan@gamil.com>
2023-08-02 11:51:36 +0530

    file system is used in sync and async mode

---
+++ fsAsync/index.js
@@ -0,0 +1,24 @@
+const fs= require('fs');
+
+// fs.mkdir("ramit",(err)=>{
+//     console.log("folder created");
+// })
+
+// fs.writeFile('read.txt',"Welcome to the world of node",
+//     (err)=>{
+//         console.log("file is created");
+//     });
+
+
+// fs.appendFile('read.txt'," Great to be here",(err)=>{
+//     console.log("Data added");
+// })
+
+
+// fs.readFile('read.txt','utf-8',(err,data)=>{
+//     console.log(data);
+// })
+
+// fs.rmdir('ramit',(err)=>{
+//     console.log("folder deleted");
+// });
