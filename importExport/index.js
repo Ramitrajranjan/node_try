31be36ad21b35276d0f6b0a456e71d5265f4c4e6
Ramit Ranjan <ramitrajranjan@gamil.com>
2023-08-03 11:55:23 +0530

    various methods and variables invoked in index.js

--- importExport/index.js
+++ importExport/index.js
@@ -1,1 +1,6 @@
-console.log(add(15,5));
+const oper= require('./operator')
+console.log(oper.add(15,5));
+console.log(oper.sub(15,5));
+console.log(oper.mul(15,5));
+console.log(oper.div(15,5));
+console.log(oper.name);
