d0ffe97ffa422b1382dae19d1a5c28a12c9eba69
Ramit Ranjan <ramitrajranjan@gamil.com>
2023-08-03 10:53:45 +0530

    path methods in node is used

---
+++ path/index.js
@@ -0,0 +1,5 @@
+const path =require('path')
+console.log(path.dirname('F:/Node_JS_Try/path/index.js'));
+console.log(path.extname('F:/Node_JS_Try/path/index.js'));
+console.log(path.basename('F:/Node_JS_Try/path/index.js'));
+console.log(path.parse('F:/Node_JS_Try/path/index.js'));
