e1cbb72fcb749d5a7385947bf5932807b9af6abd
Ramit Ranjan <ramitrajranjan@gamil.com>
2023-08-03 12:32:31 +0530

    npm nodemon is installed

--- npmMod/index.js
+++ npmMod/index.js
@@ -1,6 +1,10 @@
 const chalk = require('chalk')
+const validator = require('validator')
 console.log(chalk.blue('Hello world!'));
 console.log(chalk.blue.inverse('Hello world!'));
 console.log(chalk.blue.italic('Hello world!'));
 console.log(chalk.blue.underline('Hello world!'));
 console.log(chalk.blue.inverse.underline('Hello world!'));
+
+const res=validator.isEmail("welcomehome@gmail.com")
+console.log(res?chalk.green.inverse(res):chalk.red.inverse(res))
