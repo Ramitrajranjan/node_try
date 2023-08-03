5aa7af91ebd547bd4316b6b20a4155a56884e568
Ramit Ranjan <ramitrajranjan@gamil.com>
2023-08-03 12:19:01 +0530

    chalk features is used

--- npmMod/index.js
+++ npmMod/index.js
@@ -1,2 +1,6 @@
 const chalk = require('chalk')
 console.log(chalk.blue('Hello world!'));
+console.log(chalk.blue.inverse('Hello world!'));
+console.log(chalk.blue.italic('Hello world!'));
+console.log(chalk.blue.underline('Hello world!'));
+console.log(chalk.blue.inverse.underline('Hello world!'));
