dd323e696d2a9cb6ce600cb6081b0983f322b25d
Ramit Ranjan <ramitrajranjan@gamil.com>
2023-08-03 11:54:52 +0530

    various methods and variables created in operartor.js

--- importExport/operator.js
+++ importExport/operator.js
@@ -1,3 +1,14 @@
-const add=(a,b)=>{
-    return a+b;
-}
+const add = (a,b) => {
+    return a + b;
+};
+const sub = (a,b) => {
+    return a - b;
+};
+const mul = (a,b) => {
+    return a * b;
+};
+const div = (a,b) => {
+    return a / b;
+};
+const name='ramit';
+module.exports={add,sub,mul,div,name};
